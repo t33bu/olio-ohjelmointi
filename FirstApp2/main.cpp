68dfee8da94350b34cd4a7027347af0e734e2eab
Teemu <tjlepp@gmail.com>
2024-01-12 12:34:06 +0200

    teht1 vastattu

---
+++ FirstApp2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void terveiset(int num) {
+    cout << "Terveiset" << endl;
+    // lisää koodia
+}
+
+int main()
+{
+    terveiset(4);
+    return 0;
+}
+
