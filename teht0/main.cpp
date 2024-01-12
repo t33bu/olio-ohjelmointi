a733e71a4e0ddea9ab56b8b475b114849e24a8eb
Teemu <tjlepp@gmail.com>
2024-01-12 13:07:07 +0200

    firstapp poistettu

--- teht0/main.cpp
+++ teht0/main.cpp
@@ -3,16 +3,19 @@
 
 using namespace std;
 
+void tarkista(int num) {
+
+}
+
 int main()
 {
-    srand(time(NULL));
-    int satluku = rand() % 40;
-    cout << satluku << endl;
-    satluku = rand() % 40;
-    cout << satluku << endl;
-    satluku = rand() % 40;
-    cout << satluku << endl;
-
-    cout << "muuttunut" << endl;
+    /*
+    Laadi funktio void tarkista(int num),
+    jossa vertaillaan parametrin arvoa ja
+    tulostetaan sen mukaista tietoa ruudulle. Ohjelma siis:
+    - tulostaa ruudulle sanan "positiivinen", jos num > 0
+    - tulostaa ruudulle sanan "negatiivinen", jos num < 0
+    - tulostaa ruudulle sanan "nolla", jos num = 0
+      */
     return 0;
 }
